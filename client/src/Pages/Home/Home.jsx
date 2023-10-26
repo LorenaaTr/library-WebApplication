230a6a75d4e3351bab93ccedea70a0e4efa783a3
RinesaGrabovci8 <rinesagrabovci08@gmail.com>
2023-10-27 02:58:25 +0300

    .

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../../Components/Navbar/Navbar'
 import './home.css'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
@@ -8,7 +9,7 @@ const Home = () => {
         <Navbar/>
         <div className='moto'>
             <p className='lateststories'>LATEST  STORIES</p>
-            <p>Register for full access to read stories from Shelf Share.</p>
+            <p><Link to='/register'>Register</Link> for full access to read stories from Shelf Share.</p>
             <span className='redline'></span>
         </div>
     </div>
