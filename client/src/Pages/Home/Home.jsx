02f6510835effdde8c178d0c0733664e5fccbc2f
RinesaGrabovci8 <rinesagrabovci08@gmail.com>
2023-10-27 02:50:14 +0300

    .

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import Navbar from '../../Components/Navbar/Navbar'
+import './home.css'
 
 const Home = () => {
   return (
     <div className='home'>
         <Navbar/>
-        <div>Home</div>
+        <div className='moto'>
+            <p className='lateststories'>LATEST  STORIES</p>
+            <p>Register for full access to read stories from Shelf Share.</p>
+            <span className='redline'></span>
+        </div>
     </div>
+
   )
 }
 
