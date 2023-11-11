80241b6243eb70b61216c1718d02ba9e94ab80f1
RinesaGrabovci8 <rinesagrabovci08@gmail.com>
2023-11-12 01:55:55 +0200

    .

--- client/src/Components/Menu/Menu.jsx
+++ client/src/Components/Menu/Menu.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import './menu.css';
 import { Link } from 'react-router-dom';
 import { styled } from '@mui/material/styles';
@@ -7,52 +7,52 @@ import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 
 const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
 }));
-  
+
 const Menu = () => {
   return (
     <div className='menu'>
-        <div className='button'>
-            <button><Link to='/'>MENU</Link></button>
-        </div>
-        <div className='contextt' style={{marginTop:'150px', marginLeft:'230px',display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%'}}>
+      <div className='button'>
+        <button><Link to='/'>MENU</Link></button>
+      </div>
+      <div className='menucontext'>
+      <div className='contextt'>
         <Box sx={{ flexGrow: 1 }}>
-            <Grid container spacing={2} columns={16}>
-                <Grid item xs={8} style={{marginTop:'20px'}}>
-                    <Link to="/" style={{ textDecoration: 'none', color: 'white', fontSize:'30px' }}>HOME</Link>
+          <div className='rootdiv' >
+            <div className='firstdiv' >
+              <Grid container spacing={2} columns={16}>
+                <Grid item xs={16} >
+                  <Link to="/">HOME</Link>
+                  <Link to='/ourbooks'>OUR BOOKS</Link>
+                  <Link to='/ourpartners'>OUR PARTNERS</Link>
+                  <Link to='/ourservices'>OUR SERVICES</Link>
                 </Grid>
-                <Grid item xs={8} style={{marginTop:'20px'}}>
-                    <Link to='/privacyplocy' style={{ textDecoration: 'none', color: 'white', fontSize:'30px'  }}>PRIVACY POLICY</Link>
+              </Grid>
+            </div>
+            <div className='seconddiv' >
+              <Grid container spacing={2} columns={16}>
+                <Grid item xs={16}>
+                  <Link to='/privacypolicy'>PRIVACY POLICY</Link>
+                  <Link to='/cookiepolicy'>COOKIE POLICY</Link>
+                  <Link to='/joinasereader'>JOIN AS A READER</Link>
+                  <Link to='/joinasapartner'>JOIN AS A PARTNER</Link>
                 </Grid>
-                <Grid item xs={8} style={{marginTop:'20px'}}>
-                    <Link to='/ourbooks' style={{ textDecoration: 'none', color: 'white',fontSize:'30px'  }}>OUR BOOKS</Link>
-                </Grid>
-                <Grid item xs={8} style={{marginTop:'20px'}}>
-                    <Link to='/cookiepolicy' style={{ textDecoration: 'none', color: 'white',fontSize:'30px'  }}>COOKIE POLICY</Link>
-                </Grid>
-                <Grid item xs={8} style={{marginTop:'20px'}}>
-                    <Link to='/ourpartners' style={{ textDecoration: 'none', color: 'white',fontSize:'30px'  }}>OUR PARTNERS</Link>
-                </Grid>
-                <Grid item xs={8} style={{marginTop:'20px'}}>
-                    <Link to='/joinasereader' style={{ textDecoration: 'none', color: 'white',fontSize:'30px'  }}>JOIN AS A READER</Link>
-                </Grid>
-                <Grid item xs={8} style={{marginTop:'20px'}}>
-                    <Link to='/ourservices' style={{ textDecoration: 'none', color: 'white',fontSize:'30px'  }}>OUR SERVICES</Link>
-                </Grid>
-                <Grid item xs={8} style={{marginTop:'20px'}}>
-                    <Link to='/joinasapartner' style={{ textDecoration: 'none', color: 'white',fontSize:'30px'  }}>JOIN AS A PARTNER</Link>
-                </Grid>
-            </Grid>
+              </Grid>
+            </div>
+          </div>
         </Box>
-        <p>Copyright © 2023 Shelf Share Society | Copyright © 2023 Shelf Share Partners , LLC. All rights reserved</p>
-        </div>
+      </div>
+      <div className='footer'>
+        <p>Copyright © 2023 Shelf Share Society | Copyright © 2023 Shelf Share Partners, LLC. All rights reserved</p>
+      </div>
+      </div>
     </div>
   )
 }
 
-export default Menu
+export default Menu;
