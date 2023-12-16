99905cf368824491c25daef680baccdbb90460c1
Bleonit Shillova <bs56041@ubt-uni.net>
2023-12-16 20:29:19 +0100

    PARTNERS.

--- Server/Server/Data/Migrations/20231216190203_InitialMigrationn.Designer.cs
+++ Server/Server/Data/Migrations/20231216190203_InitialMigrationn.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Server.Context;
@@ -11,9 +12,11 @@ using Server.Context;
 namespace Server.Data.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    partial class ApplicationDBContextModelSnapshot : ModelSnapshot
+    [Migration("20231216190203_InitialMigrationn")]
+    partial class InitialMigrationn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
