434e5e70aa288fcc09f857136168c1dcc0270a0d
RinesaGrabovci8 <rinesagrabovci08@gmail.com>
2023-12-16 23:17:49 +0200

    .

--- Server/Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ Server/Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+188990045cb4b7c10e5c9b9d0adc42050110ffb9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+776369cc1ed3c0ef4a36f4e630bf359fdb1146a7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
