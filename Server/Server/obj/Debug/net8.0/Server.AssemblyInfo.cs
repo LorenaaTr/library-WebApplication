7322d550413a423b23ca4c6b8b11384dce31ce17
RinesaGrabovci8 <rinesagrabovci08@gmail.com>
2023-12-16 18:02:51 +0200

    put the users as link in header

--- Server/Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ Server/Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acbf556cf90a51bc19247ac1bca0c20323a689d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+188990045cb4b7c10e5c9b9d0adc42050110ffb9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
