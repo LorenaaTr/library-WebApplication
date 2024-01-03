e50b6be55118c8f4727f58fad4dfef351e8387ce
LorenaaTr <lorenatroshupa@gmail.com>
2024-01-03 14:50:35 +0100

    Book Categories Page

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import SystemHomePage from './Pages/SystemHomePage/HomePage';
 import SystemHeader from './Components/SystemHeader/SystemHeader';
 import SystemSidebar from './Components/SystemSidebar/SystemSidebar';
 import LibrariesSystem from './Pages/SystemLibraries/Libraries';
+import BookCategories from './Pages/BookCategories/BookCategories';
 
 const App = () => {
   return (
@@ -28,6 +29,7 @@ const App = () => {
           <Route path="/ourServices" element={<Services />} />
           <Route path="/system-home-page" element={<SystemHomePage />} />
           <Route path="/system-libraries" element={<LibrariesSystem />} />
+          <Route path="/book-categories" element={<BookCategories />} />
         </Routes>
     </BrowserRouter>
   );
---
+++ client/src/Pages/BookCategories/BookCategories.jsx
@@ -0,0 +1,79 @@
+import React from 'react'
+import SystemSidebar from '../../Components/SystemSidebar/SystemSidebar';
+import './books.css'
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+
+const BookCategories = () =>{
+  return (
+    <>
+    <SystemSidebar/>
+    <div className='books-container'>
+    <h1 className='header'>EXPLORE BOOK TYPES</h1>
+        <div className="book-categories">
+        <div className="category">
+            <i className="fas fa-book icon fiction-icon"></i>
+            <p className="category-title">FICTION</p>
+        </div>
+
+        <div className="category">
+            <i className="fas fa-history icon non-fiction-icon"></i>
+            <p className="category-title">NON-FICTION</p>
+        </div>
+
+        <div className="category">
+            <i className="fas fa-scroll icon classics-icon"></i>
+            <p className="category-title">CLASSICS</p>
+        </div>
+
+        <div className="category">
+            <i className="fas fa-child icon children-icon"></i>
+            <p className="category-title">CHILDREN'S BOOK</p>
+        </div>
+
+        <div className="category">
+            <i className="fas fa-utensils icon cook-icon"></i>
+            <p className="category-title">COOK BOOK</p>
+        </div>
+
+        <div className="category">
+            <i className="fas fa-globe icon travel-icon"></i>
+            <p className="category-title">TRAVEL GUIDES</p>
+        </div>
+
+        <div className="category">
+            <i className="fas fa-cross icon religious-icon"></i>
+            <p className="category-title">RELIGIOUS AND SPIRITUAL BOOKS</p>
+        </div>
+
+        <div className="category">
+            <i className="fas fa-mask icon comics-icon"></i>
+            <p className="category-title">COMICS</p>
+        </div>
+
+        <div className="category">
+            <i className="fas fa-graduation-cap icon crime-philosophy-icon"></i>
+            <p className="category-title">PHILOSOPHY</p>
+        </div>
+
+        <div className="category">
+            <i className="fas fa-magnifying-glass icon academic-icon"></i>
+            <p className="category-title">ACADEMIC BOOKS</p>
+        </div>
+
+        <div className="category">
+            <i className="fas fa-film  icon crime-fantasy-icon"></i>
+            <p className="category-title">TRUE CRIME AND FANTASY</p>
+        </div>
+
+        <div className="category">
+            <i className="fas fa-gamepad  icon crime-entertainment-icon"></i>
+            <p className="category-title">ENTERTAINMENT BOOKS</p>
+        </div>
+        </div>
+    </div>
+   </>
+  )
+}
+
+export default BookCategories
