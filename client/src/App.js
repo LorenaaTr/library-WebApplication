ab13f29822d6db3194717ceb41bbfda5307b85d9
LorenaaTr <lorenatroshupa@gmail.com>
2024-01-12 12:12:14 +0100

    Create Book UI Fixed and connected to firebase storage

--- client/src/App.js
+++ client/src/App.js
@@ -12,9 +12,9 @@ import SystemHomePage from './Pages/SystemHomePage/HomePage';
 import LibrariesSystem from './Pages/SystemLibraries/Libraries';
 import BookCategories from './Pages/BookCategories/BookCategories';
 import DisplayBooks from './Pages/DisplayBooks/book';
-import BookCRUD from '../src/CRUDS/BooksCrudd/BookDashboard';
-import AddBook from '../src/CRUDS/BooksCrudd/AddBook';
-import EditBook from '../src/CRUDS/BooksCrudd/EditBook';
+//import BookCRUD from '../src/CRUDS/BooksCrudd/BookDashboard';
+import CreateBook from '../src/CRUDS/BooksCrud/CreateBook';
+//import EditBook from '../src/CRUDS/BooksCrudd/EditBook';
 import Cart from './Pages/ShoppingCart/Cart';
 import { UsersProvider } from './Redux/Products/Products';
 import UserAccount from './Pages/UserAccount/UserAccount';
@@ -47,9 +47,7 @@ const App = () => {
           <Route path="/allBooks" element={<DisplayBooks />} />
           <Route path="/:allBooksID" element={<DisplayBooks />} />
           <Route path="/book-categories" element={<BookCategories />} />
-          <Route path="/book-dashboard" element={<BookCRUD />} />
-          <Route path="/add-book" element={<AddBook/>} />
-          <Route path="/edit-book/:id" element={<EditBook/>} />
+          <Route path="/create-book" element={<CreateBook/>} />
           <Route path='/useraccount' element={<UserAccount/>}/>
           <Route path='/updateusername' element={<ChangeUsername/>}/>
           <Route path='/updatepassword' element={<ChangePassword/>}/>
@@ -58,7 +56,6 @@ const App = () => {
           <Route path='/updatebirthday' element={<ChangeBirthday/>}/>
           <Route path='/updateemail' element={<ChangeEmail/>}/>
           <Route path='/updatecity' element={<ChangeCity/>}/>
-           <Route path="/edit-book/:id" element={<EditBook/>} />
           <Route path='/useraccount' element={<UserAccount/>}/>
         </Routes>
         </BrowserRouter>
--- client/src/CRUDS/BooksCrud/CreateBook.jsx
+++ client/src/CRUDS/BooksCrud/CreateBook.jsx
@@ -1,23 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SystemHeader from '../../Components/SystemHeader/SystemHeader';
 import SystemSidebar from '../../Components/SystemSidebar/SystemSidebar';
 import TextField from '@mui/material/TextField';
 import Input from '@mui/material/Input';
 import './createbook.css';
-import { Button } from '@mui/material';
-import { getStorage } from 'firebase/storage';
+import { Alert, Button } from '@mui/material';
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
+import { app } from '../../firebase';
+import CircularProgress from '@mui/material/CircularProgress';
+
 
 
 export default function CreateBook() {
   const[ file, setFile ] = useState(null);
+  const[imageUploadProgress, setImageUploadProgress] = useState(null);
+  const[imageUploadError, setImageUploadError] = useState(null);
+  const[formData, setFormData] = useState({});
   
   const handleUploadImage = async () => {
     try {
       if(!file){
+        setImageUploadError('Please select an image!');
         return;
       }
+      setImageUploadError(null);
       const storage = getStorage(app);
+      const fileName = new Date().getTime() + '-' + file.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        'state_changed',
+        (snapshot) =>{
+          const progress = 
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          setImageUploadProgress(progress.toFixed(0));
+        },(error) => {
+          setImageUploadError('Image upload faild!');
+          setImageUploadProgress(null);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            setImageUploadProgress(null);
+            setImageUploadError(null);
+            setFormData({...formData, image: downloadURL});
+          });
+        }
+      );
     } catch (error) {
+      setImageUploadError('Image upload failed!');
+      setImageUploadProgress(null);
       console.log(error);
     }
   }
@@ -29,6 +60,7 @@ export default function CreateBook() {
         <div className="components comp">
           <h1>Create BOOK</h1>
           <div className="create-book-form">
+            <div className="div1">
             <form>
               <div className="form-group">
                 <TextField
@@ -85,29 +117,58 @@ export default function CreateBook() {
                   fullWidth
                 />
               </div>
-              <div className="form-group">
+              <div className="form-group fileInput">
                 <Input
                   type="file"
                   id="image"
                   inputProps={{ accept: 'image/*' }}
-                  style={{ display: 'none' }}
                   onChange={(e) => setFile(e.target.files[0]) }
                 />
                 <label htmlFor="image">
-                  <Button variant="contained" component="span" onClick={handleUploadImage}>
-                    Upload Image
+                  <Button variant="contained" component="span" onClick={handleUploadImage} disabled={imageUploadError}>
+                    {
+                      imageUploadProgress ? (
+                      <div className="upload">
+                        <CircularProgress value={imageUploadProgress} text={`${imageUploadProgress || 0}%`}/>
+                      </div>
+                      ) : ( 
+                        'Upload Image'
+                      )}
                   </Button>
                 </label>
               </div>
-
-
-              <Button type='submit'
+            </form>
+           <div className="button-div">
+           <Button type='submit'
                     variant="contained"
                     color="error"
                     className="book-button">Create Book
             </Button>
-            </form>
+           </div>
+            </div>
+
+            <div className="div2">
+              {
+                  imageUploadError && (
+                    <Alert severity='error'>
+                      {imageUploadError}
+                    </Alert>
+                  )
+                }
+                {
+                  formData.image && (
+                    <img
+                    src={formData.image}
+                    alt='upload'
+                    className='book-picture'
+                    />
+                  )
+                }
+              </div>
           </div>
+
+         
+
         </div>
       </div>
     </>
