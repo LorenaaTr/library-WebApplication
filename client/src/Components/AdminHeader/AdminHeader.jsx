a7289fbf5fa7deaeeb9170dc566b99ecca87129b
RinesaGrabovci8 <rinesagrabovci08@gmail.com>
2024-01-14 04:06:22 +0200

    .

---
+++ client/src/Components/AdminHeader/AdminHeader.jsx
@@ -0,0 +1,93 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/images/logo.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch, faUser } from '@fortawesome/free-solid-svg-icons';
+import './adminheader.css';
+import TextField from '@mui/material/TextField';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import { useNavigate } from 'react-router-dom';
+
+const AdminHeader = () => {
+  const [isSearchVisible, setIsSearchVisible] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const toggleSearch = () => {
+    setIsSearchVisible(!isSearchVisible);
+  };
+
+  const handleUserMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleUserMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem('user');
+    navigate('/loginpartner');
+  };
+
+  return (
+    <div className='systemheader'>
+      <div className='logo'>
+        <Link to='/'>
+          <img src={logo} alt="logo" />
+        </Link>
+      </div>
+      {/* <div className={`search ${isSearchVisible ? 'active' : ''}`}>
+        <TextField
+          sx={{
+            '& label.Mui-focused': {
+              color: 'black',
+            },
+            '& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline': {
+              borderColor: 'black',
+            },
+            '& .MuiInputBase-input': {
+              color: 'black', 
+            },
+          }}
+          id="outlined-basic"
+          label="Search"
+          variant="outlined"
+          size="small"
+          fullWidth
+          InputProps={{
+            endAdornment: (
+              <IconButton>
+                <SearchIcon />
+              </IconButton>
+            ),
+          }}
+        />
+      </div> */}
+      <Button id='useracc' onClick={handleUserMenuOpen}>
+        <div className="user-icon">
+        <FontAwesomeIcon style={{ color: "black" }} icon={faUser} />
+        </div>
+      </Button>
+      <Menu
+        anchorEl={anchorEl}
+        open={Boolean(anchorEl)}
+        onClose={handleUserMenuClose}
+      >
+        <MenuItem onClick={handleUserMenuClose}>
+          <Link to='/partneraccount' style={{textDecoration:"none", color:"black"}}>Library Account</Link>
+        </MenuItem>
+        <MenuItem onClick={handleLogout}>
+          <Link to='/loginpartner' style={{textDecoration:"none", color:"black"}}>Log Out</Link>
+        </MenuItem>
+      </Menu>
+    </div>
+  );
+}
+
+export default AdminHeader;
--- client/src/Pages/AdminWeb/AdminHome/AdminHome.jsx
+++ client/src/Pages/AdminWeb/AdminHome/AdminHome.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import SystemHeader from '../../../Components/SystemHeader/SystemHeader'
 import AdminSidebar from '../../../Components/AdminSidebar/AdminSidebar'
+import AdminHeader from '../../../Components/AdminHeader/AdminHeader'
 
 const AdminHome = () => {
   return (
     <>
-    <SystemHeader/>
+    <AdminHeader/>
     <AdminSidebar/>
     <div>AdminHome</div>
     </>
