bc465352a5fc9b871df7ac956eba1eb9b89552a7
RinesaGrabovci8 <rinesagrabovci08@gmail.com>
2024-01-14 03:59:00 +0200

    .

---
+++ client/src/Pages/AdminWeb/AdminHome/AdminHome.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import SystemHeader from '../../../Components/SystemHeader/SystemHeader'
+
+const AdminHome = () => {
+  return (
+    <>
+    <SystemHeader/>
+    <div>AdminHome</div>
+    </>
+  )
+}
+
+export default AdminHome
