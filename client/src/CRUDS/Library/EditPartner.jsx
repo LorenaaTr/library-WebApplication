4d53fe3848102f567a3ba052e3bce90dca3b11ee
LorenaaTr <lorenatroshupa@gmail.com>
2024-01-18 13:10:18 +0100

    ..

--- client/src/CRUDS/Library/EditPartner.jsx
+++ client/src/CRUDS/Library/EditPartner.jsx
@@ -1,15 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { TextField, Button, Paper, Typography, Box } from '@mui/material';
-import { useParams } from 'react-router';
-import { useNavigate } from 'react-router-dom';
-import AdminHeader from '../../Components/AdminHeader/AdminHeader';
-import AdminSidebar from '../../Components/AdminSidebar/AdminSidebar';
+import React, { useEffect, useState } from 'react';
+import Input from '@mui/material/Input';
+import './editpartner.css';
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
+import { app } from '../../firebase';
+import { useNavigate, useParams } from 'react-router';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import axios from 'axios';
+import AdminHeader from '../../Components/AdminHeader/AdminHeader';
+import AdminSidebar from '../../Components/AdminSidebar/AdminSidebar';
+import { Alert, Button,TextField } from '@mui/material';
 
-const EditPartner = () => {
-  const { id } = useParams();
+
+export default function EditPartner() {
+  const [file, setFile] = useState(null);
+  const [imageUploadProgress, setImageUploadProgress] = useState(null);
+  const [imageUploadError, setImageUploadError] = useState(null);
   const [partnerData, setPartnerData] = useState({
     username: '',
     name: '',
@@ -21,9 +29,10 @@ const EditPartner = () => {
     password: '',
     image: '',
   });
+  const [publishError, setPublishError] = useState(null);
+  const { id } = useParams();
 
   const navigate = useNavigate();
-
   const notify = (message) => {
     toast.success(message, {
       autoClose: 2000,
@@ -31,6 +40,7 @@ const EditPartner = () => {
     });
   };
 
+  
   useEffect(() => {
     // Fetch partner data by ID and populate the state
     axios.get(`http://localhost:5000/partner/allpartners/${id}`)
@@ -38,9 +48,58 @@ const EditPartner = () => {
       .catch(error => console.error('Error fetching partner data:', error));
   }, [id]);
 
+  const handleUploadImage = async () => {
+    try {
+      if (!file) {
+        setImageUploadError('Please select an image!');
+        return;
+      }
+      setImageUploadError(null);
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + '-' + file.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        'state_changed',
+        (snapshot) => {
+          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          setImageUploadProgress(progress.toFixed(0));
+        },
+        (error) => {
+          setImageUploadError('Image upload failed!');
+          setImageUploadProgress(null);
+        },
+        async () => {
+          try {
+            // Get download URL after successful upload
+            const downloadURL = await getDownloadURL(uploadTask.snapshot.ref);
+            setImageUploadProgress(null);
+            setImageUploadError(null);
+            setPartnerData({ ...partnerData, image: downloadURL });
+          } catch (urlError) {
+            console.error('Error getting download URL:', urlError);
+            setImageUploadError('Image upload failed!');
+            setImageUploadProgress(null);
+          }
+        }
+      );
+    } catch (error) {
+      setImageUploadError('Image upload failed!');
+      setImageUploadProgress(null);
+      console.error(error);
+    }
+  };
 
+  const handleInputChange = (e) => {
+    setPartnerData({
+      ...partnerData,
+      [e.target.name]: e.target.value,
+    });
+  };
 
+  
   const handleUpdatePartner = () => {
+    
     axios.put(`http://localhost:5000/partner/updatepartner/${id}`, partnerData)
       .then(response => {
         console.log('Partner updated successfully:', response.data);
@@ -52,105 +111,135 @@ const EditPartner = () => {
       });
   };
 
-  const handleInputChange = (e) => {
-    setPartnerData({
-      ...partnerData,
-      [e.target.name]: e.target.value,
-    });
-  };
-
   return (
     <>
       <AdminHeader />
       <AdminSidebar />
-      <div className='singlepage'>
-        <div className='singlepagecontainer'>
-          <Box id='box'>
-            <Paper elevation={3} style={{ padding: '20px', maxWidth: '400px', margin: 'auto', marginTop: '20px' }}>
-              <Typography variant="h6" gutterBottom>
-                Update Partner
-              </Typography>
-              <TextField
-                label="Username"
-                name="username"
-                value={partnerData.username}
-                onChange={handleInputChange}
-                fullWidth
-                margin="normal"
-              />
-              <TextField
-                label="Name"
-                name="name"
-                value={partnerData.name}
-                onChange={handleInputChange}
-                fullWidth
-                margin="normal"
-              />
-              <TextField
-                label="CEO"
-                name="ceo"
-                value={partnerData.ceo}
-                onChange={handleInputChange}
-                fullWidth
-                margin="normal"
-              />
-              <TextField
-                label="City"
-                name="city"
-                value={partnerData.city}
-                onChange={handleInputChange}
-                fullWidth
-                margin="normal"
-              />
-              <TextField
-                label="State"
-                name="state"
-                value={partnerData.state}
-                onChange={handleInputChange}
-                fullWidth
-                margin="normal"
-              />
-              <TextField
-                label="Street"
-                name="street"
-                value={partnerData.street}
-                onChange={handleInputChange}
-                fullWidth
-                margin="normal"
-              />
-              <TextField
-                label="Zipcode"
-                name="zipcode"
-                value={partnerData.zipcode}
-                onChange={handleInputChange}
-                fullWidth
-                margin="normal"
-              />
-              <TextField
-                label="Password"
-                name="password"
-                type="password"
-                value={partnerData.password}
-                onChange={handleInputChange}
-                fullWidth
-                margin="normal"
-              />
-              {/* Add additional fields as needed based on your partner model */}
-              
-              <Button
-                variant="contained"
-                color="primary"
-                onClick={handleUpdatePartner}
-                style={{ marginTop: '20px' }}
-              >
-                Update Partner
-              </Button>
-            </Paper>
-          </Box>
+      <div className="home">
+        <div className="components comp">
+          <h1>Update PARTNER</h1>
+          <div className="create-book-form">
+            <div className="div1 inputs1">
+            <form>
+                <TextField
+                  label="Username"
+                  name="username"
+                  value={partnerData.username}
+                  onChange={handleInputChange}
+                  fullWidth
+                  margin="normal"
+                />
+                <TextField
+                  label="Name"
+                  name="name"
+                  value={partnerData.name}
+                  onChange={handleInputChange}
+                  fullWidth
+                  margin="normal"
+                />
+                <TextField
+                  label="CEO"
+                  name="ceo"
+                  value={partnerData.ceo}
+                  onChange={handleInputChange}
+                  fullWidth
+                  margin="normal"
+                />
+                <TextField
+                  label="City"
+                  name="city"
+                  value={partnerData.city}
+                  onChange={handleInputChange}
+                  fullWidth
+                  margin="normal"
+                />
+                <TextField
+                  label="State"
+                  name="state"
+                  value={partnerData.state}
+                  onChange={handleInputChange}
+                  fullWidth
+                  margin="normal"
+                />
+                <TextField
+                  label="Street"
+                  name="street"
+                  value={partnerData.street}
+                  onChange={handleInputChange}
+                  fullWidth
+                  margin="normal"
+                />
+                <TextField
+                  label="Zipcode"
+                  name="zipcode"
+                  value={partnerData.zipcode}
+                  onChange={handleInputChange}
+                  fullWidth
+                  margin="normal"
+                />
+                <div className="form-group fileInput">
+                  <Input
+                    type="file"
+                    id="image"
+                    accept="image/*"
+                    onChange={(e) => setFile(e.target.files[0])}
+                  />
+                  <br /> <br />
+                  <label htmlFor="image">
+                    <Button
+                      variant="contained"
+                      component="span"
+                      onClick={handleUploadImage}
+                      disabled={imageUploadProgress ? true : false}
+                      className="upload-btn"
+                    >
+                      {imageUploadProgress ? (
+                        <div className="upload">
+                          <CircularProgressbar
+                            value={imageUploadProgress}
+                            text={`${imageUploadProgress || 0}%`}
+                          />
+                        </div>
+                      ) : (
+                        'Upload Image'
+                      )}
+                    </Button>
+                  </label>
+                </div>
+
+                <div className="button-div">
+                <Button
+                    variant="contained"
+                    color="error"
+                    className="book-button"
+                    onClick={handleUpdatePartner}
+                  >
+                    Update Partner
+                  </Button>
+                    <br />
+                    {publishError && <Alert color="error">{publishError}</Alert>}
+                </div>
+              </form>
+            </div>
+            <br />
+
+            <div className="div2">
+              {imageUploadError && (
+                <Alert severity="error" className="erro">
+                  {imageUploadError}
+                </Alert>
+              )}
+              {partnerData.image && (
+                <img
+                  src={partnerData.image}
+                  alt="upload"
+                  className="book-picture1"
+                />
+              )}
+            </div>
+          </div>
         </div>
       </div>
     </>
   );
-};
-
-export default EditPartner;
+}
